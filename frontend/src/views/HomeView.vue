9c02016989a38b7d26f1a39abac0f6bd28851f8c
jakeyjakeyy <jrichca1@gmail.com>
2024-05-27 12:37:36 -0700

    basic nav component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import { useUploadStore } from "@/stores/upload";
 import Upload from "@/components/Upload.vue";
 import Selector from "@/components/Selector.vue";
+import Nav from "@/components/Nav.vue";
 const uploadStore = useUploadStore();
 </script>
 
 <template>
   <main>
+    <Nav />
     <Upload
       v-if="
         uploadStore.legendsxml == null || uploadStore.legendsplusxml == null
